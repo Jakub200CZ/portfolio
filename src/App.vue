6c7f4132e4c588518060fb3a536db985d2d08686
Jakub200CZ <kubka.sedlacek04@gmail.com>
2022-09-12 17:56:56 +0200

    Formate App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,34 @@
 <template>
   <div class="bgG text-lg">
+    <profile />
 
-    <profile/>
-  
-    <buttons/>
+    <buttons />
 
-    <work/> 
+    <work />
 
-    <aboutme/>
-    
-    <newsletter/>
+    <aboutme />
 
-    <contact/>
+    <newsletter />
 
+    <contact />
 
-    <bottom/>
-
+    <bottom />
   </div>
 </template>
 
 
 
 <script>
-import buttons from './components/buttons.vue'
-import profile from './components/profile.vue'
-import work from './components/work.vue'
-import aboutme from './components/aboutme.vue'
-import contact from './components/contact.vue'
-import bottom from  './components/bottom.vue'
-import newsletter from './components/newsletter.vue'
+import buttons from "./components/buttons.vue";
+import profile from "./components/profile.vue";
+import work from "./components/work.vue";
+import aboutme from "./components/aboutme.vue";
+import contact from "./components/contact.vue";
+import bottom from "./components/bottom.vue";
+import newsletter from "./components/newsletter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     buttons,
     profile,
@@ -39,41 +36,55 @@ export default {
     aboutme,
     contact,
     bottom,
-    newsletter
-  }
-}
+    newsletter,
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
 * {
-  font-family: 'Nunito', sans-serif;
+  font-family: "Nunito", sans-serif;
   color: #fff;
   scroll-behavior: smooth;
 }
 .bgG {
-background-color: #000000;
-background-image: linear-gradient(to right, #611f9b, #2f018b);
-
+  background-color: #000000;
+  background-image: linear-gradient(to right, #611f9b, #2f018b);
 }
 
 hr.style-two {
-    border: 0;
-    height: 1px;
-    background-image: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.374), rgba(255, 255, 255, 0));
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 0.374),
+    rgba(255, 255, 255, 0)
+  );
 }
 
 hr.style-one {
-    border: 0;
-    height: 1px;
-    background-image: linear-gradient(to right, rgba(255, 255, 255, 0.374), rgba(255, 255, 255, 0.374), rgba(255, 255, 255, 0));
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0.374),
+    rgba(255, 255, 255, 0.374),
+    rgba(255, 255, 255, 0)
+  );
 }
 
 hr.style-three {
-    border: 0;
-    height: 1px;
-    background-image: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.374), rgba(255, 255, 255, 0.374));
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 0.374),
+    rgba(255, 255, 255, 0.374)
+  );
 }
 
 .right {
